20c335edfea60e03d6e6bdeed241fa3e872b6930
Matt Wins <matt@draft9.com>
2011-03-24 16:15:44 +0000

    Initial import for Rails 3 Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raptor::Application.config.secret_token = 'a59e45059ad308f59e0bca5a131e74828d8d32ad6aab4160ace67170f52a7ebe5c4110aff8232e163de8b2f7fe7f965fd1589a463191a6547b44e74afa7cf4b8'
